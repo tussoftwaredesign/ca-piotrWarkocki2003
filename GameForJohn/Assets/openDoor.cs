2a3e4e70df56664e6db0dcf5ad1a55598dcfe5b9
Piotr Warkocki <bobstewart924@gmail.com>
2023-10-23 11:02:02 +0100

    keys open doors!

--- GameForJohn/Assets/openDoor.cs
+++ GameForJohn/Assets/openDoor.cs
@@ -7,34 +7,54 @@ public class openDoor : MonoBehaviour
 {
 
     public GameObject doorBedroom15A;
+    public GameObject key15A;
 
     public GameObject doorBedroom15B;
+    public GameObject key15B;
 
     public GameObject doorStorageRoom;
-
+    public GameObject keyStorage;
+    
     public GameObject doorLivingRoom;
-
+    public GameObject keyLivingRoom;
+    
     public GameObject doorBoilerRoom;
+    public GameObject keyBoilerRoom;
 
     public GameObject doorOutside;
 
     private void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.collider.tag == "bedroom15aDoor")
+        if (collision.collider.tag == "15A")
         {
             doorBedroom15A.SetActive(false);
+            key15A.SetActive(false);
         }
 
-        if (collision.collider.tag == "bedroom15bDoor")
+        if (collision.collider.tag == "15B")
         {
             doorBedroom15B.SetActive(false);
+            key15B.SetActive(false);
         }
 
 
         if (collision.collider.tag == "storageDoor")
         {
             doorStorageRoom.SetActive(false);
+            keyStorage.SetActive(false);
+        }
+
+        if (collision.collider.tag == "livingRoomDoor")
+        {
+            doorStorageRoom.SetActive(false);
+            keyLivingRoom.SetActive(false);
+        }
+
+        if (collision.collider.tag == "boilerDoor")
+        {
+            doorStorageRoom.SetActive(false);
+            keyBoilerRoom.SetActive(false);
         }
 
 
