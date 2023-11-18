5b7e93578a0023730658b0b5efc661a88b2eebfa
Piotr Warkocki <A00297278@student.tus.ie>
2023-11-18 21:27:57 +0000

    hamster ai set up with nav meshes and attach transforms added to dishes and keys

--- GameForJohn/Assets/hamsterFollow.cs
+++ GameForJohn/Assets/hamsterFollow.cs
@@ -6,55 +6,14 @@ using UnityEngine.AI;
 
 public class hamsterFollow : MonoBehaviour
 {
-    public GameObject target;
-    public Transform target1;
-
-    public float speed = 15.0f;
-    public float rotatespeed = 40f;
-    public float chaseRange;
-    public NavMeshAgent agent;
+   
+    public Transform player;
+    public NavMeshAgent hamster;
 
     // Update is called once per frame
     void Update()
     {
+        hamster.SetDestination(player.position);
 
-        float speedDelta = speed * Time.deltaTime;
-        Vector3 direction = target1.position - transform.position;
-
-        // Vector3 randdirection = new Vector3(Random.Range(1000, -1000), Random.Range(1000, -1000), 0);
-        float distance = direction.magnitude;
-
-        if (distance < chaseRange)
-
-        {
-
-            Ray movePosition = Camera.main.ScreenPointToRay(direction);
-
-            if (Physics.Raycast(movePosition, out var hitInfo))
-            {
-
-                agent.SetDestination(hitInfo.point);
-                //Quaternion rotation = Quaternion.LookRotation(hitInfo.point);
-                //transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotatespeed * Time.deltaTime);
-            }
-
-            // transform.position += transform.forward * speed * Time.deltaTime;
-        }
-        else
-        {
-            speed = 0;
-        }
-
-    }
-
-
-
-    void FaceTarget()
-    {
-        var turnTowardNavSteeringTarget = agent.steeringTarget;
-
-        Vector3 direction = (turnTowardNavSteeringTarget - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
     }
 }
